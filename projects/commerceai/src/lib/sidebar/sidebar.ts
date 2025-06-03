d9e29c502b8cc0fc2a0c751d6decc94dd6b13e18
Sumitk99 <sumit2k19kumar@gmail.com>
2025-06-03 13:17:52 +0530

    Restructuring

--- projects/commerceai/src/lib/sidebar/sidebar.ts
+++ projects/commerceai/src/lib/sidebar/sidebar.ts
@@ -16,47 +16,8 @@ import { MaterialModule } from '../modules/material.module';
     MarkdownModule,
     NgIf,
   ],
-  template: `
-    <mat-toolbar color="primary">
-      <span class="toolbar-title">Chats</span>
-    </mat-toolbar>
-    <mat-nav-list>
-      <mat-list-item *ngFor="let chat of chatNames">
-        <div matLine>{{chat}}</div>
-      </mat-list-item>
-    </mat-nav-list>
-  `,
-  styles: `
-    mat-toolbar {
-      display: flex;
-      align-items: center;
-    }
-
-    .toolbar-title {
-      margin-left: 8px;
-      font-weight: 600;
-    }
-
-    :host {
-      width: 260px;
-      height: 100vh;
-      display: block;
-      border-right: 1px solid #ccc;
-      background: #fafafa;
-    }
-
-    mat-nav-list {
-      padding: 0;
-    }
-
-    mat-list-item {
-      cursor: pointer;
-    }
-
-    mat-list-item:hover {
-      background: #eee;
-    }
-  `
+  templateUrl: './sidebar.html',
+  styleUrl: './sidebar.css',
 })
 export class Sidebar {
   chatNames = ['Session 1', 'Session 2', 'Session 3'];
