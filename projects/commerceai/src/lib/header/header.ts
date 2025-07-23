8d499b570185a3a2eb143566e39b9fe74869b776
Nishajoshi13 <nishajoshi132001@gmail.com>
2025-07-23 13:55:56 +0530

    profile initial fix

--- projects/commerceai/src/lib/header/header.ts
+++ projects/commerceai/src/lib/header/header.ts
@@ -1,9 +1,14 @@
-import { Component,ViewEncapsulation,Output,EventEmitter } from '@angular/core';
+import {
+  Component,
+  ViewEncapsulation,
+  Output,
+  EventEmitter,
+} from '@angular/core';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { HostListener } from '@angular/core';
-import { NgClass, NgFor, NgIf} from '@angular/common';
+import { NgClass, NgFor, NgIf } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { MatIcon } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
@@ -16,7 +21,6 @@ import { MatCard } from '@angular/material/card';
 import { UserService } from '../user.service';
 import { ViewChild } from '@angular/core';
 import { MatSidenav } from '@angular/material/sidenav';
-
 
 interface LoginResponse {
   account: {
@@ -35,25 +39,25 @@ interface LoginResponse {
     MatFormFieldModule,
     MatSelectModule,
     MatSelectModule,
-    NgClass,NgIf,NgFor,
+    NgClass,
+    NgIf,
+    NgFor,
     MatInputModule,
     MatIcon,
     MatMenuModule,
     MatButtonModule,
     MatToolbar,
-    MatCard
+    MatCard,
   ],
   templateUrl: './header.html',
   styleUrl: './header.css',
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
 })
-
-
 export class Header {
   @Output() menuClicked = new EventEmitter<void>();
   onMenuClick() {
-  this.menuClicked.emit();
-}
+    this.menuClicked.emit();
+  }
   modelMap: { model: string; domain: string }[] = [];
   selectedModel: string = '';
   selectedDomain: string = '';
@@ -69,25 +73,24 @@ export class Header {
     'http://localhost:8004',
   ];
   selectedIndex: number = 0;
-   constructor(
+  constructor(
     private http: HttpClient,
     private router: Router,
     private dialog: MatDialog,
     private userService: UserService
   ) {}
-ngOnInit(): void {
-  this.userService.user$.subscribe(user => {
-    this.userInitial = user?.initial || '';
-  });
-  if (!sessionStorage.getItem('jwt')) {
-    this.getToken(); 
-  } else {
-    this.fetchAccountDetails(); 
+  ngOnInit(): void {
+    this.userService.user$.subscribe((user) => {
+      this.userInitial = user?.initial || '';
+    });
+    if (!sessionStorage.getItem('jwt')) {
+      this.getToken();
+    } else {
+      this.fetchAccountDetails();
+    }
+    this.fetchModels();
+    document.addEventListener('click', this.closeModelDropdown.bind(this));
   }
-  this.fetchModels();
-  document.addEventListener('click', this.closeModelDropdown.bind(this));
-}
-
 
   fetchModels() {
     this.domain = this.domains[this.selectedIndex];
@@ -123,74 +126,70 @@ ngOnInit(): void {
   }
 
   @HostListener('document:click', ['$event'])
-closeModelDropdown(event: Event) {
-  const target = event.target as HTMLElement;
+  closeModelDropdown(event: Event) {
+    const target = event.target as HTMLElement;
 
-  const clickedOnModel = target.closest('.model-selector');
-  const clickedOnProfile = target.closest('[mat-menu-trigger-for]');
+    const clickedOnModel = target.closest('.model-selector');
+    const clickedOnProfile = target.closest('[mat-menu-trigger-for]');
 
-  // Don't close if clicked on model selector or profile trigger
-  if (!clickedOnModel && !clickedOnProfile) {
-    this.dropdownOpen = false;
+    if (!clickedOnModel && !clickedOnProfile) {
+      this.dropdownOpen = false;
+    }
   }
-}
 
-goToSettings() {
-  this.dialog.open(SettingsDialog, {
-    width: '950px',
-    height:'600px',
-    maxWidth: 'none',
-    panelClass: 'setting-dialog-model'
-  });
-}
-getToken() {
-  const loginData = {
-    email: 'alice.smith123@example.com',
-    password: 'newsecurepass123',
-  };
-
-  this.http
-    .post<LoginResponse>(`https://micro-scale.software/api/login`, loginData)
-    .subscribe({
-      next: (response) => {
-        sessionStorage.setItem('jwt', response.JWT_Token);
-        this.fetchAccountDetails(); 
-      },
-      error: (err) => {
-        console.error('Login error:', err);
-      },
+  goToSettings() {
+    this.dialog.open(SettingsDialog, {
+      width: '950px',
+      height: '600px',
+      maxWidth: 'none',
+      panelClass: 'setting-dialog-model',
     });
-}
+  }
+  getToken() {
+    const loginData = {
+      email: 'alice.smith123@example.com',
+      password: 'newsecurepass123',
+    };
 
+    const emailInitial = loginData.email[0].toUpperCase();
+    this.userInitial = emailInitial;
+
+    this.http
+      .post<LoginResponse>(`https://micro-scale.software/api/login`, loginData)
+      .subscribe({
+        next: (response) => {
+          sessionStorage.setItem('jwt', response.JWT_Token);
+          this.fetchAccountDetails();
+        },
+        error: (err) => {
+          console.error('Login error:', err);
+        },
+      });
+  }
 
   fetchAccountDetails() {
-  const token = sessionStorage.getItem('jwt');
-  if (!token) return;
+    const token = sessionStorage.getItem('jwt');
+    if (!token) return;
 
-  this.http.get<any>('https://micro-scale.software/api/account', {
-    headers: {
-      Authorization: token,
-    },
-  }).subscribe({
-    next: (res) => {
-      const name = res?.account?.name || '';
-      this.userInitial = this.getInitials(name);
-      this.userService.setUser({
-        name: name,
-        email: res.account.email,
-        initial: this.userInitial
+    this.http
+      .get<any>('https://micro-scale.software/api/account', {
+        headers: {
+          Authorization: token,
+        },
+      })
+      .subscribe({
+        next: (res) => {
+          // console.log('Response:', res);
+          const name = res?.account?.name || '';
+          this.userService.setUser({
+            name: name,
+            email: res.account.email,
+            initial: this.userInitial,
+          });
+        },
+        error: (err) => {
+          console.error('Error fetching account info:', err);
+        },
       });
-    },
-    error: (err) => {
-      console.error('Error fetching account info:', err);
-    },
-  });
-}
-getInitials(name: string): string {
-  const names = name.trim().split(' ');
-  const first = names[0]?.charAt(0).toUpperCase() || '';
-  const second = names[1]?.charAt(0).toUpperCase() || '';
-  return first + second;
-}
-
+  }
 }
